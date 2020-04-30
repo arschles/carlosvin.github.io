aa58a114eeef21394d9ffd1748530aa91ffbef3a
Aaron Schlesinger <aaron@ecomaz.net>
2020-04-30 22:05:24 +0000

    adding support for IDs in sized col

--- src/components/SizedCol.svelte
+++ src/components/SizedCol.svelte
@@ -1,8 +1,12 @@
 <script>
 export let cols = 8
 export let offset = 2
+export let id
 </script>
 
-<div class="offset-sm-{offset} col-sm-{cols}">
+<div
+    class="offset-sm-{offset} col-sm-{cols}"
+    id="{id}"
+>
     <slot></slot>
 </div>
